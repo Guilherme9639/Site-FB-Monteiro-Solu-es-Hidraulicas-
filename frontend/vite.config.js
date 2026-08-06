c03b8a5d7dd41316c43ef9295fef4f2aae1029af
Guilherme Almeida <cardozoalmeidaguilherme@gmail.com>
2026-08-06 13:02:21 -0300

    Separa contato e prioriza atendimento pelo WhatsApp

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         main: resolve(directory, "index.html"),
         admin: resolve(directory, "admin.html"),
         sobre: resolve(directory, "sobre.html"),
+        contato: resolve(directory, "contato.html"),
       },
     },
   },
